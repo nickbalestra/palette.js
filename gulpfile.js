bc139d14ea79d0512f77ede1de9124b3a803861a
Nick Balestra <nick@balestra.ch>
2015-08-04 17:43:06 -0700

    run mocha tests both on node and on the browser via phantom-js automate via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,20 @@
 var gulp = require('gulp'),
-    coffee = require('gulp-coffee'),
-    gutil = require('gulp-util'),
-    watch = require('gulp-watch'),
     mocha = require('gulp-mocha'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
     mochaPhantomJS = require('gulp-mocha-phantomjs');
 
-// Watch on default, build non uglified and run mocha tests
-//gulp.task('default', function() {
-//  gulp.watch(['./**/*.coffee'], ['build', 'test']);
-//});
+
 
 // Run mocha tests
-gulp.task('test', function () {
+gulp.task('test-node', function () {
   return gulp.src('./test/spec-node.js', {read: false})
     .pipe(mocha({reporter: 'nyan'}));
 });
 
-
-// Build compiles coffeeScript into javaScript
-gulp.task('build', function() {
-  gulp.src('./**/*.coffee')
-      .pipe(coffee({bare: true}).on('error', gutil.log))
-      .pipe(gulp.dest('./'))
+gulp.task('test-browser', function () {
+    return gulp
+    .src('./test/SpecRunner.html')
+    .pipe(mochaPhantomJS({reporter: 'spec'}));
 });
 
-// Release, run the build, the tests and on success generate a minified version
-gulp.task('release', ['build', 'test'], function(){
-  gulp.src('./ctree.js')
-      .pipe(uglify())
-      .pipe(rename('ctree.min.js'))
-      .pipe(gulp.dest('./'))
-  gutil.log(gutil.colors.green('Stuff happened...', 'Really, it did'));
-});
+gulp.task('test', ['test-node', 'test-browser']);
 
-
-gulp.task('test-watch', function() {
-  gulp.watch(['./**/*.js'], ['test']);
-});
